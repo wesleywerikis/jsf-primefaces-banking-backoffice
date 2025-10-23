614010c7fb617cc789ad6a52d99a0936e95da284
Wesley Werikis <wesleywerikis@outlook.com>
2025-10-23 10:39:21 -0300

    chore(project): add JSF/CDI/JPA base with persistence and transaction config

---
+++ src/main/java/com/bank/backoffice/util/EntityManagerProducer.java
@@ -0,0 +1,34 @@
+package com.bank.backoffice.util;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+	private EntityManagerFactory factory;
+
+	public EntityManagerProducer(EntityManagerFactory factory) {
+		this.factory = Persistence.createEntityManagerFactory("BankPU");
+	}
+
+	@Produces
+	@RequestScoped
+	public EntityManager createentitEntityManager() {
+		return this.factory.createEntityManager();
+	}
+
+	public void closeEntityManager(@Disposes EntityManager manager) {
+		if (manager.isOpen())
+			manager.close();
+		{
+
+		}
+	}
+
+}
---
+++ src/main/java/com/bank/backoffice/util/TransacionalInterceptor.java
@@ -0,0 +1,46 @@
+package com.bank.backoffice.util;
+
+import java.io.Serializable;
+
+import javax.annotation.Priority;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+@Interceptor
+@Transacional
+@Priority(Interceptor.Priority.APPLICATION)
+public class TransacionalInterceptor implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityManager manager;
+
+	public Object invoke(InvocationContext ctx) throws Exception {
+		EntityTransaction trx = manager.getTransaction();
+		boolean criador = false;
+		try {
+			if (!trx.isActive()) {
+				trx.begin();
+				trx.rollback();
+				trx.begin();
+				criador = true;
+			}
+			return ctx.proceed();
+		} catch (Exception e) {
+			if (trx != null && criador)
+				trx.rollback();
+			throw e;
+		} finally {
+			if (trx != null && trx.isActive() && criador)
+				trx.commit();
+			{
+
+			}
+		}
+	}
+
+}
